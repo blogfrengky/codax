ef1e4200d795d9ee2699c85eefe68558a08e1a92
Frengky <99988460+blogfrengky@users.noreply.github.com>
2022-02-28 17:36:28 +0700

    Create btn-font-awesome.js

---
+++ btn-font-awesome.js
@@ -0,0 +1,12 @@
+var cssId = 'myCss';  // you could encode the css path itself to generate id..
+if (!document.getElementById(cssId))
+{
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = cssId;
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css';
+    link.media = 'all';
+    head.appendChild(link);
+}
